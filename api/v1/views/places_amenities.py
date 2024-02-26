97fa6f423c338be56a642b5305a4d6c6350da19d
SCCSMARTCODE <sccsmart247@gmail.com>
2024-02-26 12:34:24 -0800

    ammenity

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 route for handling place and amenities linking
 """
@@ -17,7 +18,7 @@ def amenity_by_place(place_id):
     :param place_id: amenity id
     :return: all amenities
     """
-    fetched_obj = storage.get("Place", str(place_id))
+    fetched_obj = storage.get(Place, place_id)
 
     all_amenities = []
 
@@ -25,7 +26,7 @@ def amenity_by_place(place_id):
         abort(404)
 
     for obj in fetched_obj.amenities:
-        all_amenities.append(obj.to_json())
+        all_amenities.append(obj.to_dict())
 
     return jsonify(all_amenities)
 
@@ -40,12 +41,12 @@ def unlink_amenity_from_place(place_id, amenity_id):
     :param amenity_id: amenity id
     :return: empty dict or error
     """
-    if not storage.get("Place", str(place_id)):
+    if not storage.get(Place, str(place_id)):
         abort(404)
-    if not storage.get("Amenity", str(amenity_id)):
+    if not storage.get(Amenity, str(amenity_id)):
         abort(404)
 
-    fetched_obj = storage.get("Place", place_id)
+    fetched_obj = storage.get(Place, place_id)
     found = 0
 
     for obj in fetched_obj.amenities:
@@ -77,8 +78,8 @@ def link_amenity_to_place(place_id, amenity_id):
     :return: return Amenity obj added or error
     """
 
-    fetched_obj = storage.get("Place", str(place_id))
-    amenity_obj = storage.get("Amenity", str(amenity_id))
+    fetched_obj = storage.get(Place, str(place_id))
+    amenity_obj = storage.get(Amenity, str(amenity_id))
     found_amenity = None
 
     if not fetched_obj or not amenity_obj:
@@ -90,7 +91,7 @@ def link_amenity_to_place(place_id, amenity_id):
             break
 
     if found_amenity is not None:
-        return jsonify(found_amenity.to_json())
+        return jsonify(found_amenity.to_dict())
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
         fetched_obj.amenities.append(amenity_obj)
@@ -99,7 +100,7 @@ def link_amenity_to_place(place_id, amenity_id):
 
     fetched_obj.save()
 
-    resp = jsonify(amenity_obj.to_json())
+    resp = jsonify(amenity_obj.to_dict())
     resp.status_code = 201
 
     return resp
