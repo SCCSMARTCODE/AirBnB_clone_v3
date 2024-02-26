e555025275ca217ce0e557f791a2e5758fe6eaf9
SCCSMARTCODE <sccsmart247@gmail.com>
2024-02-26 01:19:53 -0800

    run

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -5,34 +5,36 @@ from flask import jsonify, abort, request
 from models import storage
 from models.state import State
 
-@app_views.route('/states', methods=['GET'], strict_slashes=False)
-def list_all_states():
-    """Retrieves the list of all State objects:"""
-    states = storage.all(State)
-    all_states = list(obj.to_dict() for obj in states.values())
-    return jsonify(all_states)
 
+# @app_views.route('/states', methods=['GET'], strict_slashes=False)
+# def list_all_states():
+#     """Retrieves the list of all State objects:"""
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
 @app_views.route('/states/<state_id>',
                  methods=['GET', 'PUT'],
                  strict_slashes=False)
 def list_states(state_id=None):
     """Retrieves a State object"""
-    state = storage.get(State, state_id)
-    if state is None:
-        abort(404, "Not found")
+    if state_id is None:
+        states = storage.all(State)
+        all_states = list(obj.to_dict() for obj in states.values())
+        return jsonify(all_states)
+    else:
+        state = storage.get(State, state_id)
+        if state is None:
+            abort(404, "Not found")
 
-    if request.method == 'GET':
-        return jsonify(state.to_dict())
+        if request.method == 'GET':
+            return jsonify(state.to_dict())
 
     if request.method == 'PUT':
-        data = request.get_json()
+        data = request.get_json(silent=True)
         if not data:
             abort(400, 'Not a JSON')
-        for key, value in data.items():
-            if key not in ['id', 'created_at', 'updated_at']:
-                setattr(state, key, value)
+        state.name = data.get('name', state.name)
         state.save()
         return jsonify(state.to_dict()), 200
+
 
 @app_views.route("/states", methods=["POST"], strict_slashes=False)
 def state_create():
@@ -49,6 +51,8 @@ def state_create():
     new_state = State(**state_json)
     new_state.save()
     return jsonify(new_state.to_dict()), 201
+    # check 13 - no issue
+
 
 @app_views.route("/states/<state_id>", methods=["DELETE"],
                  strict_slashes=False)
@@ -59,7 +63,7 @@ def state_delete_by_id(state_id):
     :return: empty dict with 200 or 404 if not found
     """
 
-    fetched_obj = storage.get("State", str(state_id))
+    fetched_obj = storage.get(State, state_id)
 
     if fetched_obj is None:
         abort(404)
@@ -67,4 +71,4 @@ def state_delete_by_id(state_id):
     storage.delete(fetched_obj)
     storage.save()
 
-    return jsonify({})
+    return jsonify({}), 200
