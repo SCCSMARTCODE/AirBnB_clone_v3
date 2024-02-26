ca64859c16783f4c9c6865cee9a399532691799b
SCCSMARTCODE <sccsmart247@gmail.com>
2024-02-26 07:48:44 -0800

    put

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -18,6 +18,34 @@ def cities_by_states(state_id):
     return jsonify(cities)
 
 
+@app_views.route("/states/<state_id>/cities", methods=["POST"],
+                 strict_slashes=False)
+def city_create(state_id):
+    """
+    create city route
+    param: state_id - state id
+    :return: newly created city obj
+    """
+    city_json = request.get_json(silent=True)
+    if city_json is None:
+        abort(400, 'Not a JSON')
+
+    if not storage.get("State", str(state_id)):
+        abort(404)
+
+    if "name" not in city_json:
+        abort(400, 'Missing name')
+
+    city_json["state_id"] = state_id
+
+    new_city = City(**city_json)
+    new_city.save()
+    resp = jsonify(new_city.to_json())
+    resp.status_code = 201
+
+    return resp
+
+
 @app_views.route('/cities/<city_id>',
                  methods=['GET', 'POST', 'DELETE', 'PUT'],
                  strict_slashes=False)
